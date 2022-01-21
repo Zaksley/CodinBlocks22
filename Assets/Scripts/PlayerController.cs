946d4cf80e2533ee3c7fc7016e26d4d8af65ef59
Zaksley <enzo8550@gmail.com>
2022-01-21 19:12:18 +0100

    Switch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum State {
+        LIGHT,
+        DARK,
+        DEAD, 
+    }
 
     public float moveSpeed = 5f; 
-    public float jumpSpeed = 5f; 
+    public float jumpSpeed = 12f; 
+    public float switchHeight = 2f; 
+    public int jumpDirection = 1; 
+    public State state; 
     public bool isGrounded = false; 
+
+
+    // Sprites
+    public Sprite darkSprite; 
+    public Sprite lightSprite; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = State.LIGHT; 
     }
 
     // Update is called once per frame
@@ -22,13 +35,58 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f); 
         transform.position += movement * Time.deltaTime * moveSpeed; 
+
+        if (Input.GetButtonDown("Switch") && isGrounded ) 
+        {
+            Switch(); 
+        }
     }
 
     void Jump() 
     {
         if (Input.GetButtonDown("Jump") && isGrounded )
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse); 
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpDirection * jumpSpeed), ForceMode2D.Impulse); 
+        }
+    }
+
+    void Switch() 
+    {
+        if (state == State.LIGHT)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = darkSprite; 
+            this.gameObject.GetComponent<SpriteRenderer>().flipY = true;
+            transform.position = new Vector3(transform.position.x, transform.position.y - switchHeight, transform.position.z); 
+            state = State.DARK; 
+            jumpDirection = -1; 
+        }
+
+        else if (state == State.DARK) 
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = lightSprite; 
+            this.gameObject.GetComponent<SpriteRenderer>().flipY = false;
+            transform.position = new Vector3(transform.position.x, transform.position.y + switchHeight, transform.position.z); 
+            state = State.LIGHT; 
+            jumpDirection = 1; 
+        }
+
+        // Flip gravity
+        this.gameObject.GetComponent<Rigidbody2D>().gravityScale = - this.gameObject.GetComponent<Rigidbody2D>().gravityScale; 
+        
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.tag == "Ground") 
+        {
+            isGrounded = true; 
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.collider.tag == "Ground") 
+        {
+            isGrounded = false; 
         }
     }
 }
