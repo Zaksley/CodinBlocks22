0d984618c7947e662b12b26a5a60faa861ff52ec
Zaksley <enzo8550@gmail.com>
2022-01-22 10:22:39 +0100

    Add a level 1, key and door

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps; 
+
+public class PlayerCollision : MonoBehaviour
+{
+    void Start(){
+
+    }
+
+    void Update() {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            Destroy(collision.gameObject);  
+            this.GetComponent<PlayerController>().gotKey = true; 
+        }
+
+        else if (collision.gameObject.CompareTag("Door"))
+        {
+            if (this.GetComponent<PlayerController>().gotKey)
+            {
+                //Destroy(collision.gameObject);  
+                // Go to next level 
+            }
+        }
+    }
+
+}
