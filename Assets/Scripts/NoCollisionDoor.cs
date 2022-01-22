152cae9881ace5dc9e89e7ec56e1aa94729ae62a
Zaksley <enzo8550@gmail.com>
2022-01-22 18:15:44 +0100

    Add levels and stuff

---
+++ Assets/Scripts/NoCollisionDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoCollisionDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<BoxCollider2D>().enabled = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,31 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps; 
-
+using UnityEngine.SceneManagement; 
 public class PlayerCollision : MonoBehaviour
 {
-    void Start(){
+    public GameObject door; 
+    private BoxCollider2D doorBox; 
+    public int maxLevel; 
+    private PlayerController player; 
 
+    void Start(){
+        doorBox = door.GetComponent<BoxCollider2D>(); 
+        player = this.GetComponent<PlayerController>(); 
     }
 
     void Update() {
         
     }
 
+    private void TakeKey(Collision2D collision)
+    {
+        Destroy(collision.gameObject);  
+        this.GetComponent<PlayerController>().gotKey = true; 
+        doorBox.enabled = true; 
+    }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         
-        if (collision.gameObject.CompareTag("Key"))
+        if (collision.gameObject.CompareTag("FireKey"))
         {
-            Destroy(collision.gameObject);  
-            this.GetComponent<PlayerController>().gotKey = true; 
+            if (player.state == PlayerController.State.DARK) 
+                TakeKey(collision);
+        }
+
+        else if (collision.gameObject.CompareTag("BlueKey"))
+        {
+            if (player.state == PlayerController.State.LIGHT) 
+                TakeKey(collision);
         }
 
         else if (collision.gameObject.CompareTag("Door"))
         {
             if (this.GetComponent<PlayerController>().gotKey)
             {
-                //Destroy(collision.gameObject);  
+                if (SceneManager.GetActiveScene().buildIndex != maxLevel) 
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+                else
+                {
+                    // Go endgame 
+                }
             }
         }
     }
