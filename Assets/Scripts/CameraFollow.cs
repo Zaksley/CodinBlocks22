27bf11fd118f50cd47775db2541c259593ec4589
Zaksley <enzo8550@gmail.com>
2022-01-23 12:16:06 +0100

    add camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player; 
+
+    public float smoothSpeed = 0.125f; 
+    public Vector3 offset; 
+
+    void LateUpdate()
+    {
+
+        //transform.position = player.position; 
+
+        Vector3 DesiredPosition = player.position + offset; 
+        Vector3 smoothePosition = Vector3.Lerp(transform.position, DesiredPosition, smoothSpeed * Time.fixedDeltaTime);
+        transform.position = smoothePosition;  
+    }
+
+
+}
